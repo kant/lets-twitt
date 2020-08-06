0474aae9fafa1c59d64cc29bda4fc6bb54c06c65
Dani Linux <dani.linux.97@gmail.com>
2020-08-06 13:15:51 +0200

    New Configs - Adding styles to the feeds - fixing some small typos

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -70,6 +70,6 @@ class TweetsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def tweet_params
-    params.require(:tweet).permit(:twet)
+    params.require(:tweet).permit(:tweet)
   end
 end
