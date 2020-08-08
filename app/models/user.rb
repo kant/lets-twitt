87c8e41b327cc59e80d2f830f25f546afaa4b3a3
Dani Linux <dani.linux.97@gmail.com>
2020-08-08 17:26:17 +0200

    Adding default profile picture if the user did not choose any pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
-  has_attached_file :photo
+  has_attached_file :photo, :default_url => "default-pro-pic.jpg"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_file_name :photo, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  
+  
 end
